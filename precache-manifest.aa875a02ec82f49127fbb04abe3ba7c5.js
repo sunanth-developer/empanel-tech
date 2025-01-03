ff259ed1ada41d4ad45cff0d2efe24649bfa9366
Sunanth Samala <sunanth.samala7@gmail.com>
2025-01-03 09:25:38 +0530

    Updates

---
+++ precache-manifest.aa875a02ec82f49127fbb04abe3ba7c5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ecd55c1d4d40e43f399959d35cd1f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1917237902429f74b012",
+    "url": "./static/css/main.3117b2e5.chunk.css"
+  },
+  {
+    "revision": "77908d947cc0156e8630",
+    "url": "./static/js/2.d8fc4604.chunk.js"
+  },
+  {
+    "revision": "af9aaea1c42d94b854b71680195ea242",
+    "url": "./static/js/2.d8fc4604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c5db4c21f15b4a4ef5",
+    "url": "./static/js/3.42dee211.chunk.js"
+  },
+  {
+    "revision": "1917237902429f74b012",
+    "url": "./static/js/main.34866006.chunk.js"
+  },
+  {
+    "revision": "b693e7b145fb7b222dbc",
+    "url": "./static/js/runtime-main.74064c4c.js"
+  },
+  {
+    "revision": "1123be27501300ffb1591eadafa2af58",
+    "url": "./static/media/Business.1123be27.png"
+  },
+  {
+    "revision": "11df3cdeddd9f536b81677194f870367",
+    "url": "./static/media/Quality.11df3cde.png"
+  },
+  {
+    "revision": "1f34b8eaff54d042e3e6ac1ae06407ef",
+    "url": "./static/media/Utility.1f34b8ea.png"
+  },
+  {
+    "revision": "4709ec8b37cae25620851e1b15098c14",
+    "url": "./static/media/application-development.4709ec8b.png"
+  },
+  {
+    "revision": "3b0307cf934b29fec54f6ae0a06a018c",
+    "url": "./static/media/download.3b0307cf.svg"
+  },
+  {
+    "revision": "999afd0bbebf302269144f714623f68c",
+    "url": "./static/media/icon.999afd0b.png"
+  },
+  {
+    "revision": "7924b88f3d665b42c34f0b6574b8e0ef",
+    "url": "./static/media/it-staff.7924b88f.png"
+  },
+  {
+    "revision": "5991892ce1ed7cf72d05763f47db569f",
+    "url": "./static/media/outsourcing.5991892c.png"
+  },
+  {
+    "revision": "0d1ebf025e470c662935933815f59da5",
+    "url": "./static/media/rushi1.0d1ebf02.jpg"
+  },
+  {
+    "revision": "f9d587d804e074785d2c0f89c93949ec",
+    "url": "./static/media/sunanth.f9d587d8.png"
+  }
+]);
